2f7735e5340f978065cb96fc2a55022fce6713cd
Manas Ranjan <pandamanasranjan67@gmail.com>
2025-06-23 17:38:08 +0000

    fix itttttt

--- src/next.config.ts
+++ src/next.config.ts
@@ -2,9 +2,6 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    allowedDevOrigins: ["https://*.cloudworkstations.dev"],
-  },
   /* config options here */
   images: {
     remotePatterns: [
